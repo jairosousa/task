dc54188b9dfff73245d3c02ca8dd574078cb628c
Jairo Nascimento <jaironsousa@gmail.com>
2024-05-22 16:46:21 -0300

    implementando o padrão build

--- src/main/java/br/com/jnsdev/task/model/Task.java
+++ src/main/java/br/com/jnsdev/task/model/Task.java
@@ -12,40 +12,81 @@ public class Task {
     private int priority;
     private TaskState state;
 
-    public String getTitle() {
-        return title;
+    public Task() {
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public Task(Builder builder) {
+        this.title = builder.title;
+        this.description = builder.description;
+        this.priority = builder.priority;
+        this.state = builder.state;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public int getPriority() {
         return priority;
-    }
-
-    public void setPriority(int priority) {
-        this.priority = priority;
     }
 
     public TaskState getState() {
         return state;
     }
 
-    public void setState(TaskState state) {
-        this.state = state;
-    }
-
     public Task newTask() {
         TaskService.taskList.add(this);
         return this;
+    }
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public static Builder builderFrom(Task task){
+        return new Builder(task);
+    }
+
+    public static class Builder {
+        private String title;
+        private String description;
+        private int priority;
+        private TaskState state;
+        public Builder() {
+        }
+        public Builder(Task task) {
+            this.title = task.title;
+            this.description = task.description;
+            this.priority = task.priority;
+            this.state = task.state;
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder priority(int priority) {
+            this.priority = priority;
+            return this;
+        }
+
+        public Builder state(TaskState state) {
+            this.state = state;
+            return this;
+        }
+
+        public Task build() {
+            return new Task(this);
+        }
     }
 }
